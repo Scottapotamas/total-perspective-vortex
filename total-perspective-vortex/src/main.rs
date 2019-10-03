c034137dcc97ceb96d92699a44c9c0453c246dea
Scott <scottapotamas@gmail.com>
2019-10-03 22:43:45 +0930

    Pass metadata up the chain, generate a summary json file

--- total-perspective-vortex/src/main.rs
+++ total-perspective-vortex/src/main.rs
@@ -12,6 +12,9 @@ use zaphod_export::*;
 pub mod sequencer;
 use sequencer::*;
 
+use serde::Serialize;
+use std::fs;
+
 // Checks that a DirEntry isn't hidden, a __MACOSX folder, or a file
 fn is_frame_folder(entry: &DirEntry) -> bool {
     if entry.file_type().is_dir() {
@@ -37,30 +40,59 @@ fn main() {
     println!("Welcome to the Total Perspective Vortex!");
 
     // Walk the folder structure looking for frame folders, then process them
-    WalkDir::new("./collected")
+    let meta: Vec<FrameMetadata> = WalkDir::new("./collection")
         .min_depth(1)
         .max_depth(1)
         .into_iter()
         .filter_entry(|e| is_frame_folder(e))
         .filter_map(|v| v.ok())
-        .for_each(|x| process_frame_folder(&x));
+        .map(|x| process_frame_folder(&x))
+        .collect();
+
+    let overview_file = serde_json::to_string_pretty(&meta).expect("Summary Serialisation Failed");
+
+    fs::write(Path::new("./collection/summary.json"), overview_file).expect("Unable to write file");
 }
 
 // From a valid frame folder, find collections folders to process
-fn process_frame_folder(entry: &DirEntry) {
-    println!("\nProcessing Frame {:?}", entry.file_name());
+fn process_frame_folder(entry: &DirEntry) -> FrameMetadata {
+    let frame_folder_name = format!("{}", entry.file_name().to_str().unwrap());
+    println!("\nProcessing Frame {}", frame_folder_name);
 
-    WalkDir::new(entry.path())
+    let exported_file_metadata: Vec<FileMetadata> = WalkDir::new(entry.path())
         .min_depth(1)
         .max_depth(1)
         .into_iter()
         .filter_entry(|e| e.file_type().is_dir())
         .filter_map(|v| v.ok())
-        .for_each(|x| process_collection(&x));
+        .map(|x| process_collection(&x))
+        .collect();
+
+    return FrameMetadata {
+        frame_num: frame_folder_name,
+        files: exported_file_metadata,
+    };
 }
 
-// A collection is the lowest level folder. Contains json and uv files from Blender
-fn process_collection(entry: &DirEntry) {
+#[derive(Serialize, Debug)]
+struct FrameMetadata {
+    frame_num: String,
+    files: Vec<FileMetadata>,
+}
+
+#[derive(Serialize, Debug)]
+struct FileMetadata {
+    collection: String,
+    toolpath_path: String,
+    duration: u32,
+    first_move: u32,
+    last_move: u32,
+    viewer_vertices_path: String,
+    viewer_uv_path: String,
+}
+
+// A collection is the deepest level folder. Contains json and (optional) uv files from Blender
+fn process_collection(entry: &DirEntry) -> FileMetadata {
     // Parse all the json files in the current directory
     let mut parsed_splines: Vec<IlluminatedSpline> = WalkDir::new(entry.path())
         .min_depth(1)
@@ -82,12 +114,23 @@ fn process_collection(entry: &DirEntry) {
             // todo support closed splines
         }
 
-        // Perform any LED manipulation here
+        // Perform
+        // LED manipulation here
         // TODO consider supporting color inversion?
     }
 
     // Take our spline+illumination data, and generate a tool-path
     let planned_events = sequence_events(parsed_splines);
+
+    let file_duration: u32 = planned_events
+        .delta
+        .iter()
+        .map(|x| x.payload.duration)
+        .sum();
+
+    //let move_range : (u32, u32)
+    let first = planned_events.delta.first().unwrap().payload.id;
+    let last = planned_events.delta.last().unwrap().payload.id;
 
     // Add header information
     let output_data: DeltaEvents = DeltaEvents {
@@ -101,9 +144,12 @@ fn process_collection(entry: &DirEntry) {
         .file_name()
         .expect("Couldn't get collection name")
         .to_str()
-        .expect("Failed converting collection name to string");
+        .expect("Failed converting collection name to string")
+        .to_string();
 
-    let file_name = format!("{}_toolpath.json", collection_name).to_lowercase();
+    let mut file_name = format!("{}_toolpath.json", collection_name).to_lowercase();
+    file_name.retain(|c| !c.is_whitespace()); //strip whitespace
+
     let export_json_path = Path::new(&file_name);
 
     let destination_folder = entry.path().parent().unwrap();
@@ -111,4 +157,16 @@ fn process_collection(entry: &DirEntry) {
 
     // Write to the JSON file in format suitable for zaphod-bot
     export_toolpath(destination_path.as_path(), output_data);
+
+    let metadata = FileMetadata {
+        collection: collection_name,
+        toolpath_path: destination_path.into_os_string().into_string().unwrap(),
+        duration: file_duration,
+        first_move: first,
+        last_move: last,
+        viewer_vertices_path: "blah.json".to_string(),
+        viewer_uv_path: "blah.uv".to_string(),
+    };
+
+    return metadata;
 }
